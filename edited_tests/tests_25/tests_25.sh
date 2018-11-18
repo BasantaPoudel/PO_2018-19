02be480be1acc4e74517d858d11f6b0c714b66e6
ricardo-7 <ricardo.de.almeida@tecnico.ulisboa.pt>
2018-11-18 13:54:04 +0000

    my test

--- edited_tests/tests_25/tests_25.sh
+++ edited_tests/tests_25/tests_25.sh
@@ -10,11 +10,25 @@ fi
 
 for FILE_RAW in $(ls auto-tests/*.in);
 do
+
+
 	FILE=$(echo $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
-	echo Testing $FILE...
+
+	echo executing $FILE
 	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App	
-	echo "Testing now the result:"
-	diff -b auto-tests/expected/$FILE.out results/$FILE.outhyp
+	if diff auto-tests/expected/$FILE.out results/$FILE.outhyp >/dev/null ; then
+		echo $FILE PASSED
+	else
+		echo $FILE FAILED
+		echo please schedule a fix
+		echo
+		echo $FILE 
+		diff -b auto-tests/expected/$FILE.out results/$FILE.outhyp
+		echo 
+		cat auto-tests/expected/$FILE.out
+		cat results/$FILE.outhyp
+	fi
+	
 done	
 
 cd $PROJECTDIR && make clean &> /dev/null && cd ../tests/tests_25
