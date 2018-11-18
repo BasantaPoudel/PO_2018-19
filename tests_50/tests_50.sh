5fbd9e692d417c1899a28f611bfac13643aa300f
ricardo-7 <ricardo.de.almeida@tecnico.ulisboa.pt>
2018-11-18 13:17:50 +0000

    fixed commit removal

--- tests_50/tests_50.sh
+++ tests_50/tests_50.sh
@@ -1,7 +1,7 @@
 PROJECTDIR=../project
 
 echo "Compiling..."
-cd $PROJECTDIR && make &> /dev/null && cd ../Tests
+cd $PROJECTDIR && make &> /dev/null && cd ../tests_50
 echo "Done compiling!"
 
 if [ ! -d results/ ]; then
@@ -12,10 +12,10 @@ for FILE_RAW in $(ls auto-tests/*.in);
 do
 	FILE=$(echo $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
 	echo Testing $FILE...
-	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App	
+	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App
 	echo "Testing now the result:"
 	diff -b auto-tests/expected/$FILE.out results/$FILE.outhyp
-done	
+done
 
 cd $PROJECTDIR && make clean &> /dev/null && cd ../Tests
 
