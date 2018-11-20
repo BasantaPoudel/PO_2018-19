71f2c7760ce6f0afd7014ceba4ddde9dcbd88d0f
ricardo-7 <ricardo.de.almeida@tecnico.ulisboa.pt>
2018-11-20 21:18:16 +0000

    tests edited

---
+++ edited-tests/tests_25_r/test.sh
@@ -0,0 +1,46 @@
+PROJECTDIR=../../project
+
+echo -e "Compiling..."
+cd $PROJECTDIR && make &> /dev/null && cd ../edited-tests/tests_25_r
+echo -e "Done compiling!"
+
+if [ ! -d results/ ]; then
+	mkdir results
+fi
+
+for FILE_RAW in $(ls auto-tests/*.in);
+do
+
+
+	FILE=$(echo -e $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
+
+	echo -e executing $FILE
+	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App	
+	
+	if diff auto-tests/expected/$FILE.out results/$FILE.outhyp >/dev/null ; then
+		echo -e $FILE PASSED
+	else
+		echo -e $"______________________________________________________________________________________________________________"
+		echo -e $FILE FAILED
+		cat descr/$FILE.descr
+		echo -e please schedule a fix
+		echo -e $FILE 
+		echo -e $"______________________________________________________________________________________________________________"
+		cat auto-tests/$FILE.import
+		echo -e $"______________________________________________________________________________________________________________"
+		cat auto-tests/$FILE.in
+		echo -e $"______________________________________________________________________________________________________________"
+		diff -b -y results/$FILE.outhyp auto-tests/expected/$FILE.out
+		# echo -e $"ours ______________________________________________________________________________________________________________"
+		# cat results/$FILE.outhyp ; echo
+		# echo -e $"correct ______________________________________________________________________________________________________________"
+		# cat auto-tests/expected/$FILE.out ; echo
+		# echo -e $"______________________________________________________________________________________________________________"
+		
+	fi
+
+done	
+cd $PROJECTDIR && make clean &> /dev/null && cd ../edited-tests/tests_25_r
+
+notify-send "done"
+echo -e "Done!"
