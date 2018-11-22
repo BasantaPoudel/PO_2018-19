d42fc39b43dffa35093bbbfed6e03bde6fe5a555
Basanta <basanta.poudel@tecnico.ulisboa.pt>
2018-11-22 13:55:25 +0000

    Form Parse for some commands added.

--- project/sth-app/src/sth/app/teaching/DoCloseProject.java
+++ project/sth-app/src/sth/app/teaching/DoCloseProject.java
@@ -12,20 +12,28 @@ import sth.SchoolManager;
  */
 public class DoCloseProject extends Command<SchoolManager> {
 
-  //FIXME add input fields if needed
+  //FIXME[FIXING-BEGIN] add input fields if needed
+  Input<String> _projectname;
+  Input<String> _disciplinename;
+  //FIXME[FIXING-END] add input fields if needed
 
   /**
    * @param receiver
    */
   public DoCloseProject(SchoolManager receiver) {
     super(Label.CLOSE_PROJECT, receiver);
-    //FIXME initialize input fields if needed
+    //FIXME [FIXING-BEGIN] initialize input fields if needed
+    _disciplinename = _form.addStringInput(Message.requestDisciplineName());
+    _projectname = _form.addStringInput(Message.requestProjectName());
+    //FIXME [FIXING-END] initialize input fields if needed  }
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() throws DialogException {
     //FIXME implement command
+    _form.parse();
+
   }
 
 }
--- project/sth-app/src/sth/app/teaching/DoCreateProject.java
+++ project/sth-app/src/sth/app/teaching/DoCreateProject.java
@@ -12,19 +12,26 @@ import sth.SchoolManager;
  */
 public class DoCreateProject extends Command<SchoolManager> {
 
-  //FIXME add input fields if needed
+  //FIXME[FIXING-BEGIN] add input fields if needed
+  Input<String> _projectname;
+  Input<String> _disciplinename;
+  //FIXME[FIXING-END] add input fields if needed
 
   /**
    * @param receiver
    */
   public DoCreateProject(SchoolManager receiver) {
     super(Label.CREATE_PROJECT, receiver);
-    //FIXME initialize input fields if needed
+    //FIXME [FIXING-BEGIN] initialize input fields if needed
+    _disciplinename = _form.addStringInput(Message.requestDisciplineName());
+    _projectname = _form.addStringInput(Message.requestProjectName());
+    //FIXME [FIXING-END] initialize input fields if needed  }
   }
-
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() throws DialogException {
+    _form.parse();
+
     //FIXME implement command
   }
 
