532dbfdc73684c9bd8fa6cfa12415eb44ec40597
ricardo-7 <ricardo.de.almeida@tecnico.ulisboa.pt>
2018-11-23 10:02:41 +0000

    tests

---
+++ tests2/tests_50/ptest.sh
@@ -0,0 +1,52 @@
+PROJECTDIR=../../project
+v2=tests2
+v=tests_50
+
+echo -e "Compiling..."
+cd $PROJECTDIR && make &> /dev/null && cd ../$v2/$v
+echo -e "Done compiling!"
+
+if [ ! -d results/ ]; then
+	mkdir results
+fi
+
+for FILE_RAW in $(ls auto-tests/*.in);
+do
+
+
+	FILE=$(echo -e $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
+
+	echo -e executing $FILE
+	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App
+
+
+	echo -e $"______________________________________________________________________________________________________________"
+	if diff auto-tests/expected/$FILE.out results/$FILE.outhyp >/dev/null ; then
+		
+	else
+		echo -e $FILE FAILED
+
+
+		#____________________________________________________________________________________________________________________________________________________________
+		echo -e $"______________________________________________________________________________________________________________" > log/$FILE.log
+		echo -e $FILE FAILED >> log/$FILE.log
+		cat desc/$FILE.desc >> log/$FILE.log
+		echo -e please schedule a fix >> log/$FILE.log
+		echo -e $FILE  >> log/$FILE.log
+		echo -e $"______________________________________________________________________________________________________________" >> log/$FILE.log
+		cat auto-tests/$FILE.import >> log/$FILE.log
+		echo -e $"______________________________________________________________________________________________________________" >> log/$FILE.log
+		cat auto-tests/$FILE.in >> log/$FILE.log
+		echo -e $"______________________________________________________________________________________________________________" >> log/$FILE.log
+		cat auto-tests/expected/$FILE.out >> log/$FILE.log
+		echo -e $"______________________________________________________________________________________________________________" >> log/$FILE.log
+		diff -b -y results/$FILE.outhyp auto-tests/expected/$FILE.out >> log/$FILE.log
+		#____________________________________________________________________________________________________________________________________________________________
+
+	fi
+
+done
+cd $PROJECTDIR && make clean &> /dev/null && cd ../$v2/$v
+
+notify-send "done"
+echo -e "Done!"
--- tests2/tests_50/tests_50.sh
+++ tests2/tests_50/tests_50.sh
@@ -1,8 +1,9 @@
 PROJECTDIR=../../project
+v=tests_25-e
 
-echo "Compiling..."
-cd $PROJECTDIR && make &> /dev/null && cd ../tests/tests_50
-echo "Done compiling!"
+echo -e "Compiling..."
+cd $PROJECTDIR && make &> /dev/null && cd ../tests-e/$v
+echo -e "Done compiling!"
 
 if [ ! -d results/ ]; then
 	mkdir results
@@ -10,14 +11,37 @@ fi
 
 for FILE_RAW in $(ls auto-tests/*.in);
 do
-	FILE=$(echo $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
-	echo Testing $FILE...
+
+
+	FILE=$(echo -e $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
+
+	echo -e executing $FILE
 	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App
-	echo "Testing now the result:"
-	diff -b auto-tests/expected/$FILE.out results/$FILE.outhyp
+
+	if diff auto-tests/expected/$FILE.out results/$FILE.outhyp >/dev/null ; then
+		echo -e $FILE PASSED
+	else
+		echo -e $"______________________________________________________________________________________________________________"
+		echo -e $FILE FAILED
+		cat desc/$FILE.desc
+		echo -e please schedule a fix
+		echo -e $FILE
+		echo -e $"______________________________________________________________________________________________________________"
+		cat auto-tests/$FILE.import
+		echo -e $"______________________________________________________________________________________________________________"
+		cat auto-tests/$FILE.in
+		echo -e $"______________________________________________________________________________________________________________"
+		diff -b -y results/$FILE.outhyp auto-tests/expected/$FILE.out
+		# echo -e $"ours ______________________________________________________________________________________________________________"
+		# cat results/$FILE.outhyp ; echo
+		# echo -e $"correct ______________________________________________________________________________________________________________"
+		# cat auto-tests/expected/$FILE.out ; echo
+		# echo -e $"______________________________________________________________________________________________________________"
+
+	fi
+
 done
+cd $PROJECTDIR && make clean &> /dev/null && cd ../tests-e/$v
 
-cd $PROJECTDIR && make clean &> /dev/null && cd ../tests/tests_50
-
-
-echo "Done!"
+notify-send "done"
+echo -e "Done!"
