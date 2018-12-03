76431577c44a56e158e248ecd7be5ad6248108e9
ricardo-7 <ricardo.de.almeida@tecnico.ulisboa.pt>
2018-12-03 21:49:00 +0000

    tests

---
+++ tests2/tests_50/list.sh
@@ -0,0 +1,68 @@
+PROJECTDIR=../../project
+v2=tests2
+v=tests_50
+
+if [ ! -d results/ ]; then
+	mkdir results
+fi
+
+echo "" > log/A-00.log
+echo "" > log/A-00wik.log
+
+for FILE_RAW in $(ls auto-tests/*.in);
+do
+
+	FILE=$(echo $FILE_RAW | grep -o -P "A-\d{2}-\d{2}-M-ok")
+
+	java -Dimport=auto-tests/$FILE.import -Din=auto-tests/$FILE.in -Dout=results/$FILE.outhyp -cp "/usr/share/java/po-uuilib.jar:$PROJECTDIR/sth-core/sth-core.jar:$PROJECTDIR/sth-app/sth-app.jar" sth.app.App
+
+	printf  $FILE
+	echo "" > log/$FILE.log
+
+	if diff auto-tests/expected/$FILE.out results/$FILE.outhyp >/dev/null ; then
+			 if [ -f log/$FILE.log ];then
+				 rm log/$FILE.log
+			 fi
+			echo " yes"
+
+			printf "$FILE.................. o o ">> log/A-00wik.log
+			cat desc/$FILE.desc >> log/A-00wik.log
+	else
+		printf " no";
+
+		printf "$FILE.................. o ! ">> log/A-00wik.log
+		cat desc/$FILE.desc >> log/A-00wik.log
+
+		echo $FILE  >> log/$FILE.log
+		cat desc/$FILE.desc >> log/$FILE.log
+
+		echo "_____________________ in _________________________________________________________________________________________" >> log/$FILE.log
+		cat auto-tests/$FILE.in >> log/$FILE.log
+
+		echo "__________ diff ____________________________________________________________________________________________________" >> log/$FILE.log
+		colordiff -b -y  results/$FILE.outhyp auto-tests/expected/$FILE.out >> log/$FILE.log
+
+
+		echo "________expected______________________________________________________________________________________________________" >> log/$FILE.log
+		cat auto-tests/expected/$FILE.out  >> log/$FILE.log
+
+		echo "__________us____________________________________________________________________________________________________" >> log/$FILE.log
+		cat results/$FILE.outhyp >> log/$FILE.log
+
+
+
+		cat desc/$FILE.desc
+ 
+
+
+		#____________________________________________________________________________________________________________________________________________________________
+
+
+	fi
+
+
+done
+cd $PROJECTDIR
+
+notify-send "done"
+echo "Done!"
